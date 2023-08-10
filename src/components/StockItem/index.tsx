c0594f65fa5c1b1260c62d2eb1bb770c08e7d385
admrts <alidemirtas.7@icloud.com>
2023-08-11 00:56:57 +0300

    feat: productlist design

--- src/components/StockItem/index.tsx
+++ src/components/StockItem/index.tsx
@@ -2,12 +2,28 @@ import {View, Text} from 'react-native';
 import React from 'react';
 import styles from './styles';
 
-const index = ({name = 'iphone', unitPrice = '3000', unitStock = '10'}) => {
+interface Props {
+  name: string;
+  unitPrice: number;
+  unitStock: number;
+}
+
+const index = ({name, unitPrice, unitStock}: Props) => {
   return (
-    <View style={styles.wrapper}>
-      <Text style={styles.name}>Name: {name}</Text>
-      <Text>Price: ${unitPrice}</Text>
-      <Text>in Stock: {unitStock}</Text>
+    <View
+      style={[
+        styles.wrapper,
+        {backgroundColor: unitPrice > 50 ? 'red' : 'white'},
+      ]}>
+      <Text style={[styles.name, {color: unitPrice > 50 ? 'white' : 'black'}]}>
+        Name: {name}
+      </Text>
+      <Text style={{color: unitPrice > 50 ? 'white' : 'black'}}>
+        Price: ${unitPrice}
+      </Text>
+      <Text style={{color: unitPrice > 50 ? 'white' : 'black'}}>
+        in Stock: {unitStock}
+      </Text>
     </View>
   );
 };
